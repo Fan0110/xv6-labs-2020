584d2ee1cc2ded27ac8325716de8f02c366b57f5
fancy <Fancy_Jiayi@gmail.com>
2023-08-11 19:32:20 +0800

    feat: lab5 finished

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -11,10 +11,10 @@ uint64
 sys_exit(void)
 {
   int n;
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -33,7 +33,7 @@ uint64
 sys_wait(void)
 {
   uint64 p;
-  if(argaddr(0, &p) < 0)
+  if (argaddr(0, &p) < 0)
     return -1;
   return wait(p);
 }
@@ -43,12 +43,30 @@ sys_sbrk(void)
 {
   int addr;
   int n;
+  struct proc *p;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
+
+  p = myproc();
+  addr = p->sz;
+
+  if (n >= 0 && addr + n >= addr)
+  {
+    p->sz += n; // increase size but not allocate memory
+  }
+  else if (n < 0 && addr + n >= PGROUNDUP(p->trapframe->sp))
+  {
+    // handle negative n and addr must be above user stack - lab5-3
+    p->sz = uvmdealloc(p->pagetable, addr, addr + n);
+  }
+  else
+  {
     return -1;
+  }
+
+  // if(growproc(n) < 0)
+  //   return -1;
   return addr;
 }
 
@@ -58,12 +76,14 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
       release(&tickslock);
       return -1;
     }
@@ -78,7 +98,7 @@ sys_kill(void)
 {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
